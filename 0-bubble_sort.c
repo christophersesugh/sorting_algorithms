316b730af1d8adbb0cb2017f8d8a065d7509f4a2
christophersesugh <christohybrid185@gmail.com>
2023-06-25 23:04:57 +0000

    refactor: improve bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,17 +8,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int pass, i, temp;
+	int pass, i, temp, swapped;
 
-	for (pass = size - 1; pass >= 0; pass--)
+	swapped = 1;
+	for (pass = size - 1; pass >= 0 && swapped; pass--)
 	{
 		for (i = 0; i <= pass - 1; i++)
 		{
+			swapped = 0;
 			if (array[i] > array[i + 1])
 			{
 				temp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
+				swapped = 1;
 			}
 		}
 	}
