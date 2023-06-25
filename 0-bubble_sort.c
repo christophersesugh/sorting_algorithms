3d1ddedf64cef3556d68d706fa642577d267c35e
christophersesugh <christohybrid185@gmail.com>
2023-06-25 22:46:07 +0000

    feat: complete task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - implement bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int pass, i, temp, swapped;
+	swapped = 1;
+	for (pass = size - 1; pass >= 0; pass--)
+	{
+		swapped = 0;
+		for (i = 0; i <= pass - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped = 1;
+			}
+		}
+	}
+}
---
+++ swap.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * swap - swap the values of two variables
+ * @a: first variable
+ * @b: second variable
+ * Return: nothing
+ */
+void swap(int a, int b)
+{
+	int temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
