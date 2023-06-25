88a33df734bf1e3751c8ccde866271e5a713dd2d
christophersesugh <christohybrid185@gmail.com>
2023-06-25 23:17:57 +0000

    refactor: bubble function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,11 +11,11 @@ void bubble_sort(int *array, size_t size)
 	int i, j, temp, swapped;
 
 	swapped = 1;
-	for (i = size - 1; i >=0 && swapped; i++)
+	for (i = size - 1; i >=0 && swapped; i--)
 	{
+		swapped = 0;
 		for (j = 0; j <=  size - 1; j++)
 		{
-			swapped = 0;
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
